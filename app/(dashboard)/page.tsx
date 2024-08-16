418ee9e57f020fa062b32231c20486f206086d3c
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-16 22:59:55 +0530

    Accounts GET api completed

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+"use client";
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 const DashboardPage = () => {
-  return (
-    <div>
-      DashboardPage
-    </div>
-  )
-}
+  return <div>HomePage</div>;
+};
 
-export default DashboardPage
+export default DashboardPage;
