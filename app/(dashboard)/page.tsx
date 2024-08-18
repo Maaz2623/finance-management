d4aa270edbe4985acdb25760f71902447b7abea4
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-18 20:33:13 +0530

    Accounts POST API complete
    TODO: Data table component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,9 +2,17 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
 
 const DashboardPage = () => {
-  return <div>HomePage</div>;
+  const { isOpen, onClose, onOpen } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an Account</Button>
+    </div>
+  );
 };
 
 export default DashboardPage;
