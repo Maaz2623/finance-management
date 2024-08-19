bd7a163024af20304160919f919e0a8bbc866aa0
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-19 20:44:47 +0530

    Edit Accounts Completed
    TODO: Categories (API + UI)

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -6,8 +6,12 @@ import { ArrowUpDown } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { client } from "@/lib/hono";
 import { InferResponseType } from "hono";
+import { Actions } from "./actions";
 
-export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0]
+export type ResponseType = InferResponseType<
+  typeof client.api.accounts.$get,
+  200
+>["data"][0];
 
 export const columns: ColumnDef<ResponseType>[] = [
   {
@@ -45,5 +49,9 @@ export const columns: ColumnDef<ResponseType>[] = [
         </Button>
       );
     },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <Actions id={row.original.id} />,
   },
 ];
