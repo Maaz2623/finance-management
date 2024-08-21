7c416ce13c2d737854fae9d3fab4d5f603f0ba87
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-21 21:36:14 +0530

    Completed Transactions API
    TODO: Transactions hooks

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -6,6 +6,7 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import accounts from "./accounts";
 import { HTTPException } from "hono/http-exception";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 
@@ -21,7 +22,8 @@ app.onError((err, c) => {
 
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
